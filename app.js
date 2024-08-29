b7b4d723bebdd1af8dce3373221deda7151f0117
Jibin T J <jibintj6351@gmail.com>
2024-08-29 21:37:26 +0530

    added database connection

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://miniProject:GDDOuwvLEwvoI0Pf@miniproject.i1rai.mongodb.net/?retryWrites=true&w=majority&appName=miniProject"
+  )
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.use(express.json());
 
-app.get('/api/test/', function(req, res){
-
-    res.status(200).send('Hello World');   
-
+app.get("/api/test/", function (req, res) {
+  res.status(200).send("Hello World");
 });
 
-app.listen(3000, function(){
-    console.log('Server running on port 3000');
+app.listen(3000, function () {
+  console.log("Server running on port 3000");
 });
-
