85f163af0cfdbbb99650dd1bac99491f5ca955a4
Shabab Greengardans <sgreengardans@sfgarabia.local>
2024-09-02 11:55:22 +0300

    code updated for branch

--- controllers/branchesController.js
+++ controllers/branchesController.js
@@ -72,32 +72,64 @@ async function createBranch(req, res) {
   }
 }
 
-
-
 async function updateBranch(req, res) {
   try {
+    const body = req.body;
+
+    // Filter to find the document to update
+    const filter = { branchCode: req.params.code };
+
+    // Update operation
+    const update = 
+    {
+      $set:
+      {               
+        "branchName": body.branchName,
+        "address": body.address,
+        "gstNo": body.gstNo,
+        "tinNo": body.tinNo,
+        "otherNo": body.otherNo,
+        "license1": body.license1,
+        "license1Exp": body.license1Exp,
+        "license2": body.license2,
+        "license2Exp": body.license2Exp,
+        "license3": body.license3,
+        "license3Exp": body.license3Exp,
+        "license4": body.license4,
+        "license4Exp": body.license4Exp,
+        "license5": body.license5,
+        "license5Exp": body.license5Exp,
+        "mobileNo": body.mobileNo
+      }
+    };
+    // Options for the update operation
+    const options = {
+      new: true, // Return the updated document
+      upsert: false // Create a new document if no matching document is found
+    };
+
     const result = await branch.findOne({ branchCode: Number(req.params.code) });
-    if (result != null) {
-      const data = await branch.findOneAndUpdate(
-        { branchCode: req.params.code },
-        req.body
-      );
-      res.status(200).json({ "Message": "Branch Updated" });
+
+    if (result != null) {      
+      // const data = await branch.findOneAndUpdate(filter, update, options);
+      const data = await branch.findOneAndUpdate(filter, update, options);
+      if (!data) {
+        res.status(401).json({ "Message": "Branch Not Updated" });      }
+      else {           
+        res.status(201).json({ "Message": "Branch Updated Successfully" });
+      }
     }
     else {
       res.status(409).json({ "Message": "Not a valid branch Code" });
     }
     // return res;
   } catch (err) {
+    console.log(err);
     res.status(401).json({ "Message": err })
   }
   finally {
   }
 }
-
-
-
-
 
 module.exports = {
   readAllBranch,
